bf64648cdeef3baf5efa671eca2908a88c1777a8
Hisateru Tanaka <tanakahisateru@gmail.com>
2023-10-08 08:06:07 +0900

    LoggerAwareTrait の意味がわかるコメント

--- src/SetterInjectionDemo.php
+++ src/SetterInjectionDemo.php
@@ -8,6 +8,10 @@ class SetterInjectionDemo
 {
     use LoggerAwareTrait;
 
+    // ↑ こういうの
+    // protected ?LoggerInterface $logger = null;
+    // public function setLogger(LoggerInterface $logger): void
+
     public function run(): int
     {
         $this->logger?->debug('Hello from ' . __CLASS__);
